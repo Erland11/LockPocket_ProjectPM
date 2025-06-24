628dd98772c84900bcb42d26ba26a81dd7e6ae43
reva-cmd <reva_ekalia@teknokrat.ac.id>
2025-06-24 19:05:36 +0700

    Menu Awal Aplikasi

--- app/src/main/java/com/uti/lockpocket_pm/MainActivity.kt
+++ app/src/main/java/com/uti/lockpocket_pm/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.uti.lockpocket_pm
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.app.ActivityOptions
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val options = ActivityOptions.makeCustomAnimation(
+                this,
+                android.R.anim.slide_in_left,
+                android.R.anim.slide_out_right
+            )
+            startActivity(intent, options.toBundle())
+            finish()
+        }, 2000)
     }
 }
