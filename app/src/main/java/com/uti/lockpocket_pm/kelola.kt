c827a492454206b3782916d056ccdfcd68e54e31
reva-cmd <reva_ekalia@teknokrat.ac.id>
2025-06-25 16:25:34 +0700

    menambah query agar tombol kembali dapat diklik dan kembali kemenu utama

--- app/src/main/java/com/uti/lockpocket_pm/kelola.kt
+++ app/src/main/java/com/uti/lockpocket_pm/kelola.kt
@@ -1,7 +1,12 @@
 package com.uti.lockpocket_pm
 
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -9,12 +14,21 @@ import androidx.core.view.WindowInsetsCompat
 class kelola : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_kelola)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val btnKembali = findViewById<ImageView>(R.id.btnKembali)
+        btnKembali.setOnClickListener {
+            val intent = Intent(this, utama::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
